49a7a6979ba922ec19ca0c0295dedf258d6cffeb
Raphaël Chenouard <raphaelchenouard@gmail.com>
2021-09-15 21:25:01 +0200

    Add config file for Ansible to use local dir to store hosts file.

--- ansi.sh
+++ ansi.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-
+cur_path=$(pwd)
+echo "Writing hosts to $cur_path"
 # load user configuration
 source userconfig/configuration.sh
-sudo echo "[duts]" > /etc/ansible/hosts
-sudo nmap --exclude $EXCLUDE --open -sn ${SUBNET} -oG - | awk '/Up$/{print $2}' | sort >> /etc/ansible/hosts
+echo "[duts]" > $cur_path/hosts
+sudo nmap --exclude $EXCLUDE --open -sn ${SUBNET} -oG - | awk '/Up$/{print $2}' | sort >> $cur_path/hosts
 ansible duts -a "apt update " -u root
 ansible duts -a "apt -y -qq upgrade " -u root
 ansible duts -a "reboot " -u root
