1e09f66905ae0ae93aedcdd751e4b7b926e80df7
Raphaël Chenouard <raphaelchenouard@gmail.com>
2021-09-15 22:27:46 +0200

    Add login credentials to connect to hosts

--- ansi.sh
+++ ansi.sh
@@ -4,8 +4,15 @@ cur_path=$(pwd)
 echo "Writing hosts to $cur_path"
 # load user configuration
 source userconfig/configuration.sh
+# write discovered hosts
 echo "[duts]" > $cur_path/hosts
 sudo nmap --exclude $EXCLUDE --open -sn ${SUBNET} -oG - | awk '/Up$/{print $2}' | sort >> $cur_path/hosts
+# write common vars to duts group
+echo "[duts:vars]" >> $cur_path/hosts
+echo "ansible_user=$USER_ROOT" >> $cur_path/hosts
+echo "ansible_password=$PASS_ROOT" >> $cur_path/hosts
+
+
 ansible duts -a "apt update " -u root
 ansible duts -a "apt -y -qq upgrade " -u root
 ansible duts -a "reboot " -u root
